858c03790d79eadbbd90ea2eff007277606e8f11
Robert Williams <robert.williams@speakwrite.com>
2019-05-23 14:51:08 -0500

    Refactoring and Summaries

--- BookStore.DataAccess/Interfaces/IStringDataAccess.cs
+++ BookStore.DataAccess/Interfaces/IStringDataAccess.cs
@@ -1,5 +1,8 @@
 ﻿namespace BookStore.DataAccess.Interfaces
 {
+    /// <summary>
+    /// Responsible for accessing, storing, and deleting data from a file.
+    /// </summary>
     public interface IStringDataAccess : IRetrieve, IStore, IDelete
     {
     }
